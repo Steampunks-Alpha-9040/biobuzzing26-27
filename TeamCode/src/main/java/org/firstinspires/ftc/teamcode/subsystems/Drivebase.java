8cf5d1d4ee494a47d4bb8cc6ee8370604b25146c
quakeq <thomasahn2009@gmail.com>
2026-09-18 12:08:51 -0700

    worky worky

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Drivebase.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Drivebase.java
@@ -1,63 +1,15 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
-import com.pedropathing.follower.Follower;
-import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
-import com.qualcomm.robotcore.hardware.Gamepad;
-
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
-import org.firstinspires.ftc.teamcode.Constants;
-
-import dev.nextftc.control.drive.MecanumKinematics;
+import dev.nextftc.hardware.RobotController;
 import dev.nextftc.hardware.actuators.NextMotor;
 import dev.nextftc.robot.Mechanism;
-import dev.nextftc.robot.drive.DriveCommands;
-
 
 public class Drivebase implements Mechanism {
-
-    private static Drivebase drivebase;
-
-    private NextMotor FR;
-    private NextMotor FL;
-    private NextMotor BR;
-    private NextMotor BL;
-    private GoBildaPinpointDriver odo;
-
-    private Follower follower;
-
-    private Drivebase(){
-        FR = new NextMotor(Constants.Drive.FR);
-        FL = new NextMotor(Constants.Drive.FL);
-        BR = new NextMotor(Constants.Drive.BR);
-        BL = new NextMotor(Constants.Drive.BL);
-    }
-
-    @Override
-    public void periodic() {
-        follower.update();
-    }
-
-    public void updateValues() {
-        odo.update();
-    }
-
-    public void drive(Gamepad joy){
-        DriveCommands.mecanumDriveFieldCentric(FL, FR, BL /*hehe*/, BR, joy, () -> odo.getHeading(AngleUnit.RADIANS), getMecanumKinematics());
-    }
-
-    public static Drivebase getInstance(){
-        if (drivebase == null){
-            drivebase = new Drivebase();
-        }
-        return drivebase;
-    }
-
-    private MecanumKinematics getMecanumKinematics(){
-        return new MecanumKinematics(1.1); //literally only value is to configure for strafing
-    }
-
-
-
+        public Drivebase(){}
+        public final NextMotor frontLeft = new NextMotor(RobotController.controlHub(), 0);
+        public final NextMotor frontRight = new NextMotor(RobotController.controlHub(), 1);
+        public final NextMotor backLeft = new NextMotor(RobotController.controlHub(), 2);
+        public final NextMotor backRight =  new NextMotor(RobotController.controlHub(), 3);
 
 
 
